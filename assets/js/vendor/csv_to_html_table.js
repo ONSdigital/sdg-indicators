596ed6984205cd5788046837aa56f5af9a69ebe9
Shane Porter <shane@freshclickmedia.com>
2017-02-04 10:26:03 +0000

    #my-table is no more

--- assets/js/vendor/csv_to_html_table.js
+++ assets/js/vendor/csv_to_html_table.js
@@ -1,17 +1,15 @@
 function init_table(options) {
 
   options = options || {};
-  var csv_path = options.csv_path;
-  var el = options.element || 'table-container';
-  var allow_download = options.allow_download || false;
-  var csv_options = options.csv_options || {separator: ',', delimiter: '"'};
-  var datatables_options = options.datatables_options || { paging: false, bInfo: false, searching: false};
-  var table_class = options.table_class || 'table table-hover';
+  var csv_path = options.csv_path,
+      el = options.element || 'table-container',
+      allow_download = options.allow_download || false,
+      csv_options = options.csv_options || {separator: ',', delimiter: '"'},
+      datatables_options = options.datatables_options || { paging: false, bInfo: false, searching: false},
+      table_class = options.table_class || 'table table-hover';
 
   $("#" + el).html('<table class="' + table_class + '" id="my-table"></table>');
-
-  console.info(csv_path);
-  console.info(el);
+  var table = $('#' + el).find('table');
 
   $.when($.get(csv_path)).then(
     function(data){      
@@ -24,21 +22,22 @@ function init_table(options) {
       }
 
       table_head += '</tr></thead>';
-      $('#my-table').append(table_head);
-      $('#my-table').append('<tbody></tbody>');
+      table.append(table_head);
+      table.append('<tbody></tbody>');
 
-      for (row_id = 1; row_id < csv_data.length; row_id++) { 
+      for (var row_id = 1; row_id < csv_data.length; row_id++) { 
         var row_html = '<tr>';
 
-          for (col_id = 0; col_id < csv_data[row_id].length; col_id++) { 
-            row_html += '<td>' + csv_data[row_id][col_id] + '</td>';
-          }
+        for (col_id = 0; col_id < csv_data[row_id].length; col_id++) { 
+          row_html += '<td>' + csv_data[row_id][col_id] + '</td>';
+        } 
           
         row_html += '</tr>';
-        $('#my-table tbody').append(row_html);
+
+        table.find('tbody').append(row_html);
       }
 
-      $('#my-table').DataTable(datatables_options);
+      table.DataTable(datatables_options);
 
       // if (allow_download)
       //   $("#" + el).append("<p><a class='btn btn-info' href='" + csv_path + "'><i class='glyphicon glyphicon-download'></i> Download as CSV</a></p>");
