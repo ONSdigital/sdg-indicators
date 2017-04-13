58d3e7801a534f1d66ab2f43d794fc67a97e9eaa
Shane Porter <shane@freshclickmedia.com>
2017-04-13 23:38:51 +0100

    splitting data

--- assets/js/chartDataProcessor.js
+++ assets/js/chartDataProcessor.js
@@ -6,6 +6,9 @@ var chartDataProcessor = {
         var datasets = [];
         var labels = [];
 
+        console.log('data: ', data);
+        console.log('datasetObject: ', datasetObject);
+
         for(var loop = 1; loop < Object.keys(data[0]).length; loop++) {
 
             var dataset = {
@@ -16,12 +19,29 @@ var chartDataProcessor = {
                 data: _.pluck(data, Object.keys(data[0])[loop])
             };
 
+            var ds2 = _.pluck(data, Object.keys(data[0])[loop]);
+            ds2 = _.map(ds2, function(i) { return +i + 10; });
+
+            var dataset2 = {
+                label: Object.keys(data[0])[loop] + ' ds2',
+                backgroundColor: '#' + this.colors[loop],
+                borderColor: '#' + this.colors[loop],
+                borderWidth: 1,
+                data: ds2
+
+            };
+
             datasets.push(_.isObject(datasetObject) ? _.extend(dataset, datasetObject) : dataset);
+            datasets.push(_.isObject(datasetObject) ? _.extend(dataset2, datasetObject) : dataset2);
         }
 
-        return {
+        var res = {
             datasets: datasets,
             labels: _.pluck(data, Object.keys(data[0])[0])
         };
+
+        console.log('res: ', res);
+
+        return res;
     }
 };
--- assets/js/dataManager.js
+++ assets/js/dataManager.js
@@ -1,38 +1,68 @@
-function getFields(obj) {
-	return _.filter(Object.keys(data[0]), function(key) { return ['Year', 'Value'].indexOf(key) === -1; });
-}
+var dataManager = {
 
-function allNull(obj, fields) {
-	for(var loop = 0; loop < fields.length; loop++) {
-		if(obj[fields[loop]])
-    	return false;
-  }
-  return true;
-}
+  colors: ['e5243b', '4c9f38', 'ff3a21', '26bde2', 'dd1367', 'fd9d24', '3f7e44', '00689d'],
 
-function onlyPropertySet(obj, allFields, property) {
-	for(var loop = 0; loop < allFields.length; loop++) {
-  	if(allFields[loop] === property && !obj[allFields[loop]]) {
-    	return false;	// has to have a value
-    } else if(allFields[loop] !== property && obj[allFields[loop]]) {
-    	return false;	// has to have no value set
+  getFields: function(obj) {
+    return _.filter(Object.keys(obj), function(key) { return ['Year', 'Value'].indexOf(key) === -1; });
+  },
+
+  allNull: function(obj, fields) {
+    for(var loop = 0; loop < fields.length; loop++) {
+      if(obj[fields[loop]])
+        return false;
     }
+    return true;
+  },
+
+  onlyPropertySet: function(obj, allFields, property) {
+    for(var loop = 0; loop < allFields.length; loop++) {
+      if(allFields[loop] === property && !obj[allFields[loop]]) {
+        return false;	// has to have a value
+      } else if(allFields[loop] !== property && obj[allFields[loop]]) {
+        return false;	// has to have no value set
+      }
+    }
+    return true;
+  },
+
+  getChartInfo: function(data, datasetObject) {
+
+    var datasets = [];
+    var fields = this.getFields(data[0]);
+    var that = this;
+    var seriesData;
+    var years;
+
+    [null].concat(fields).forEach(function(field, index) {
+
+      seriesData = _.chain(data)
+                .filter(function(i) { return field ? that.onlyPropertySet(i, fields, field) : that.allNull(i, fields); })
+                .sortBy(function(i) { return i.Year; })
+                .value();
+
+      if(!index) {
+        years = _.pluck(seriesData, 'Year');
+      }
+
+      datasets.push(
+          _.extend({
+            label: field ? field : 'Overall',
+            backgroundColor: '#' + that.colors[index],
+            borderColor: '#' + that.colors[index],
+            data: _.pluck(seriesData, 'Value'),
+            borderWidth: 1
+          }, datasetObject));
+    });
+
+    return {
+      datasets: datasets,
+      labels: years
+    };
   }
-  return true;
-}
 
-// overall:
-var fields = getFields(data[0]);
+  // return {
+  //   getChartInfo: getChartInfo
+  // };
 
-// each:
-[null].concat(fields).forEach(function(field) {
-	var results = {
-  								field: field ? field : 'Overall',
-                	data: _.chain(data)
-                      .filter(function(i) { return field ? onlyPropertySet(i, fields, field) : allNull(i, fields); })
-                      .sortBy(function(i) { return i.Year; })
-                      .value()
-                };
+};
 
-	console.log(results);
-});
