6a1fd49d8c87fbcdc3421400ee4002706c1f89e9
Douglas Ashton <douglas.j.ashton@gmail.com>
2017-04-17 22:19:40 +0100

    write out first and last header

--- scripts/csvcheck.rb
+++ scripts/csvcheck.rb
@@ -4,6 +4,7 @@ csvs = Dir.glob("data/indicator*.csv")
 
 csvs.each do |csv|
   dat = CSV.read(csv, :headers => true)
+  # you can get the headers so now you can error if they're wrong
   puts "#{dat.headers[0]}, #{dat.headers[-1]}"
 end
 
