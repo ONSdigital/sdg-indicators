7173c5afeadfbc47329e344856a8756642b8361d
Shane Porter <shane@freshclickmedia.com>
2017-05-11 13:49:10 +0100

    editorconfig/formatting

--- assets/js/indicatorModel.js
+++ assets/js/indicatorModel.js
@@ -1,4 +1,4 @@
-var indicatorModel = function(options) {
+var indicatorModel = function (options) {
 
   var that = this;
   this.data = options.data;
@@ -20,74 +20,88 @@ var indicatorModel = function(options) {
 
   var colors = ['777777', '0082e5', '79c3fc', '005da7', 'ff9c18', 'f47d00', 'ad8cf3', '9675e2'];
 
-  this.getSelectableFields = function(obj) {
-    return _.filter(Object.keys(obj), function(key) { return ['Year', 'Value'].indexOf(key) === -1; });
+  this.getSelectableFields = function (obj) {
+    return _.filter(Object.keys(obj), function (key) {
+      return ['Year', 'Value'].indexOf(key) === -1;
+    });
   };
 
-  this.allNull = function(obj, fields) {
-    for(var loop = 0; loop < fields.length; loop++) {
-      if(obj[fields[loop]])
+  this.allNull = function (obj, fields) {
+    for (var loop = 0; loop < fields.length; loop++) {
+      if (obj[fields[loop]])
         return false;
     }
     return true;
   };
 
-  this.onlyPropertySet = function(obj, allFields, properties) {
-    if(!_.isArray(properties)) {
+  this.onlyPropertySet = function (obj, allFields, properties) {
+    if (!_.isArray(properties)) {
       properties = [properties];
     }
 
-    for(var loop = 0; loop < allFields.length; loop++) {
-      if(properties.indexOf(allFields[loop]) !== -1 && !obj[allFields[loop]]) {
-        return false;	// has to have a value
-      } else if(properties.indexOf(allFields[loop]) === -1 && obj[allFields[loop]]) {
-        return false;	// has to have no value set
+    for (var loop = 0; loop < allFields.length; loop++) {
+      if (properties.indexOf(allFields[loop]) !== -1 && !obj[allFields[loop]]) {
+        return false; // has to have a value
+      } else if (properties.indexOf(allFields[loop]) === -1 && obj[allFields[loop]]) {
+        return false; // has to have no value set
       }
     }
     return true;
   };
 
-  this.updateSelectedFields = function(fields) {
+  this.updateSelectedFields = function (fields) {
     this.selectedFields = fields;
     this.getData();
   };
 
-  this.getData = function(initial) {
+  this.getData = function (initial) {
 
     var fields = this.selectedFields,
-        datasets = [],
-        selectableFields = this.getSelectableFields(this.data[0]),
-        that = this,
-        seriesData = [],
-        tableData = [],
-        years = _.chain(this.data).pluck('Year').uniq().sortBy(function(year) { return year; }).value(),
-        allFunc = function() {
-          return _.chain(that.data)
-            .filter(function(i) { return that.allNull(i, selectableFields); })
-            .sortBy(function(i) { return i.Year; })
-            .map(function(d) { return _.pick(d, _.identity); })
-            .value();
-        },
-        datasetIndex = 0,
-        convertToDataset = function(data, field, fieldValue) {
-          var fieldIndex = field ? _.findIndex(that.selectedFields, function(f) { return f === field; }) : undefined,
-              ds = _.extend({
+      datasets = [],
+      selectableFields = this.getSelectableFields(this.data[0]),
+      that = this,
+      seriesData = [],
+      tableData = [],
+      years = _.chain(this.data).pluck('Year').uniq().sortBy(function (year) {
+        return year;
+      }).value(),
+      allFunc = function () {
+        return _.chain(that.data)
+          .filter(function (i) {
+            return that.allNull(i, selectableFields);
+          })
+          .sortBy(function (i) {
+            return i.Year;
+          })
+          .map(function (d) {
+            return _.pick(d, _.identity);
+          })
+          .value();
+      },
+      datasetIndex = 0,
+      convertToDataset = function (data, field, fieldValue) {
+        var fieldIndex = field ? _.findIndex(that.selectedFields, function (f) {
+            return f === field;
+          }) : undefined,
+          ds = _.extend({
             label: field && fieldValue ? field + ' ' + fieldValue : 'All',
             borderColor: '#' + colors[datasetIndex],
             pointBorderColor: '#' + colors[datasetIndex],
-            data: _.map(years, function(year) {
-              var found = _.findWhere(data, { Year: year });
+            data: _.map(years, function (year) {
+              var found = _.findWhere(data, {
+                Year: year
+              });
               return found ? found.Value : null;
             }),
             borderWidth: field ? 2 : 4,
             // apply dash to secondary fields:
             borderDash: fieldIndex > 0 ? [((fieldIndex + 1) * 2), ((fieldIndex + 1) * 2)] : []
           }, that.datasetObject);
-          datasetIndex++;
-          return ds;
-        };
+        datasetIndex++;
+        return ds;
+      };
 
-    if(fields && !_.isArray(fields)) {
+    if (fields && !_.isArray(fields)) {
       fields = [].concat(fields);
     }
 
@@ -96,80 +110,106 @@ var indicatorModel = function(options) {
     tableData.push({
       title: 'Overall',
       headings: ['Year', 'Value'],
-      data: _.map(allFunc(), function(d) {
+      data: _.map(allFunc(), function (d) {
         return [d.Year, d.Value]
       })
     });
 
     // with optional fields:
-    if(_.isArray(fields)) {
-      fields.forEach(function(field) {
+    if (_.isArray(fields)) {
+      fields.forEach(function (field) {
         var data =
           _.chain(that.data)
-            .filter(function(i) { return that.onlyPropertySet(i, selectableFields, field); })
-            .sortBy(function(i) { return i.Year; })
-            .map(function(d) { return _.pick(d, _.identity); })
-            .value();
+          .filter(function (i) {
+            return that.onlyPropertySet(i, selectableFields, field);
+          })
+          .sortBy(function (i) {
+            return i.Year;
+          })
+          .map(function (d) {
+            return _.pick(d, _.identity);
+          })
+          .value();
 
         // Year, fieldvalue-1, fieldvalue-2, fieldvalue-n
-        var uniqueFieldValues = _.chain(data).pluck(field).uniq().sortBy(function(d) { return d[field]; }).value(),
-            result = _.map(years, function(year) {
-              return _.reduce(_.where(data, { 'Year': year }), function(o, v){
-                o.Year = v.Year;
-                o[v[field]] = v.Value;
-                return o;
+        var uniqueFieldValues = _.chain(data).pluck(field).uniq().sortBy(function (d) {
+            return d[field];
+          }).value(),
+          result = _.map(years, function (year) {
+            return _.reduce(_.where(data, {
+              'Year': year
+            }), function (o, v) {
+              o.Year = v.Year;
+              o[v[field]] = v.Value;
+              return o;
             }, {});
           });
 
         tableData.push({
           title: 'Breakdown by ' + field,
           headings: ['Year'].concat(uniqueFieldValues),
-          data: _.filter(data, function(d) { return d.Value; }).length ? _.map(_.filter(result, function(rr) { return Object.keys(rr).length; }), function(r) {
-              return [r.Year].concat(_.map(uniqueFieldValues, function(f) {
-                return r[f];
-              }));
+          data: _.filter(data, function (d) {
+            return d.Value;
+          }).length ? _.map(_.filter(result, function (rr) {
+            return Object.keys(rr).length;
+          }), function (r) {
+            return [r.Year].concat(_.map(uniqueFieldValues, function (f) {
+              return r[f];
+            }));
           }) : []
         });
 
         // breakdown by that field's individual series:
         _.chain(data).pluck(field).uniq().value()
-          .forEach(function(value, index) {
+          .forEach(function (value, index) {
             datasets.push(convertToDataset(
               _.chain(data)
-                .filter(function(d) { return d[field] === value; })
-                .sortBy(function(d) { return d.Year; }).value(),
+              .filter(function (d) {
+                return d[field] === value;
+              })
+              .sortBy(function (d) {
+                return d.Year;
+              }).value(),
               field,
               value
             ));
-        });
+          });
       });
 
       // combined disaggregations:
-      if(fields.length === 2) {
+      if (fields.length === 2) {
         // extract data:
         var combinedData =
-            _.chain(that.data)
-              .filter(function(i) { return that.onlyPropertySet(i, selectableFields, fields); })
-              .sortBy(function(i) { return i.Year; })
-              .map(function(d) { return _.pick(d, _.identity); })
-              .value();
+          _.chain(that.data)
+          .filter(function (i) {
+            return that.onlyPropertySet(i, selectableFields, fields);
+          })
+          .sortBy(function (i) {
+            return i.Year;
+          })
+          .map(function (d) {
+            return _.pick(d, _.identity);
+          })
+          .value();
 
         // get unique field labels:
-        var uniqueFieldCombinations = _.map(_.groupBy(combinedData,function(d){
+        var uniqueFieldCombinations = _.map(_.groupBy(combinedData, function (d) {
           return d[fields[0]] + '_' + d[fields[1]];
-        }),function(grouped){
+        }), function (grouped) {
           return _.pick(grouped[0], fields[0], fields[1]);
           //return grouped[0][fields[0]] + '_' + grouped[0][fields[1]];
         });
 
         tableData.push({
-          title: 'Breakdown by ' + fields.join( ' and '),
-          headings: ['Year'].concat(_.map(uniqueFieldCombinations, function(fc) { return fc[fields[0]] + ' and ' + fc[fields[1]]; })),
-          data: combinedData.length ? _.map(years, function(year) {
-            return [year].concat(_.map(uniqueFieldCombinations, function(ufc) {
-                ufc.Year = year;
-                var found = _.findWhere(combinedData, ufc);
-                return found ? found.Value : undefined;
+          title: 'Breakdown by ' + fields.join(' and '),
+          headings: ['Year'].concat(_.map(uniqueFieldCombinations, function (fc) {
+            return fc[fields[0]] + ' and ' + fc[fields[1]];
+          })),
+          data: combinedData.length ? _.map(years, function (year) {
+            return [year].concat(_.map(uniqueFieldCombinations, function (ufc) {
+              ufc.Year = year;
+              var found = _.findWhere(combinedData, ufc);
+              return found ? found.Value : undefined;
             }));
           }) : []
         });
@@ -185,7 +225,7 @@ var indicatorModel = function(options) {
       indicatorId: this.indicatorId
     });
 
-    if(initial) {
+    if (initial) {
       this.onSeriesComplete.notify({
         series: this.getSelectableFields(this.data[0])
       });
@@ -198,11 +238,10 @@ var indicatorModel = function(options) {
 };
 
 indicatorModel.prototype = {
-  initialise: function() {
+  initialise: function () {
     this.getData(true);
   },
-  getData: function() {
+  getData: function () {
     this.getData();
   }
 };
-
--- assets/js/indicatorView.js
+++ assets/js/indicatorView.js
@@ -1,4 +1,4 @@
-var indicatorView = function(model, options) {
+var indicatorView = function (model, options) {
 
   "use strict";
 
@@ -10,18 +10,18 @@ var indicatorView = function(model, options) {
   this._chartInstance = undefined;
   this._rootElement = options.rootElement;
 
-  this._model.onDataComplete.attach(function(sender, args) {
+  this._model.onDataComplete.attach(function (sender, args) {
     view_obj.createPlot(args);
     view_obj.createTables(args);
   });
 
-  this._model.onSeriesComplete.attach(function(sender, args) {
+  this._model.onSeriesComplete.attach(function (sender, args) {
     view_obj.initialiseSeries(args);
   });
 
-  this._model.onSeriesSelectedChanged.attach(function(sender, args) {
+  this._model.onSeriesSelectedChanged.attach(function (sender, args) {
     var selector;
-    if(args.series.length === view_obj._fieldLimit) {
+    if (args.series.length === view_obj._fieldLimit) {
       selector = $('#fields input:not(:checked)');
       selector.attr('disabled', true);
       selector.parent().addClass('disabled').attr('title', 'Maximum of ' + view_obj._fieldLimit + ' selections; unselect another to select this field');
@@ -32,18 +32,18 @@ var indicatorView = function(model, options) {
     }
   });
 
-  $(this._rootElement).on('click', 'input:checkbox', function() {
+  $(this._rootElement).on('click', 'input:checkbox', function () {
     var selectedFields = [];
 
-    $('#fields input:checked').each(function(i, field) {
+    $('#fields input:checked').each(function (i, field) {
       selectedFields.push($(field).val());
     });
 
     view_obj._model.updateSelectedFields(selectedFields);
   });
 
-  this.initialiseSeries = function(args) {
-    args.series.forEach(function(series) {
+  this.initialiseSeries = function (args) {
+    args.series.forEach(function (series) {
       $('#fields').append($('<label/>').text(series).append($('<input/>')
         .attr({
           'type': 'checkbox',
@@ -52,9 +52,9 @@ var indicatorView = function(model, options) {
     });
   };
 
-  this.createPlot = function(chartInfo) {
+  this.createPlot = function (chartInfo) {
 
-    if(this._chartInstance) {
+    if (this._chartInstance) {
       this._chartInstance.destroy();
     }
 
@@ -62,39 +62,39 @@ var indicatorView = function(model, options) {
       type: 'line',
       data: chartInfo,
       options: {
-          responsive: true,
-          maintainAspectRatio: false,
-          scales: {
-            xAxes: [{
-                gridLines: {
-                    color: '#ddd',
-                }
-            }],
-            yAxes: [{
-              ticks: {
-                  suggestedMin: 0
-              }
-            }]
-          },
-          legend: {
-            display: true,
-            usePointStyle: true
-          }
+        responsive: true,
+        maintainAspectRatio: false,
+        scales: {
+          xAxes: [{
+            gridLines: {
+              color: '#ddd',
+            }
+          }],
+          yAxes: [{
+            ticks: {
+              suggestedMin: 0
+            }
+          }]
+        },
+        legend: {
+          display: true,
+          usePointStyle: true
+        }
       }
     });
   };
 
-  this.toCsv = function(tableData) {
+  this.toCsv = function (tableData) {
     var lines = [],
-        headings = tableData.headings;
+      headings = tableData.headings;
 
     lines.push(headings.join(','));
 
-    _.each(tableData.data, function(dataValues) {
+    _.each(tableData.data, function (dataValues) {
       var line = [];
 
-      _.each(headings, function(heading, index) {
-          line.push(dataValues[index]);
+      _.each(headings, function (heading, index) {
+        line.push(dataValues[index]);
       });
 
       lines.push(line.join(','));
@@ -103,44 +103,56 @@ var indicatorView = function(model, options) {
     return lines.join('\n');
   };
 
-  this.createTables = function(chartInfo) {
+  this.createTables = function (chartInfo) {
 
     options = options || {};
     var that = this,
-        csv_path = options.csv_path,
-        el = options.element || '#datatables',
-        allow_download = options.allow_download || false,
-        csv_options = options.csv_options || {separator: ',', delimiter: '"'},
-        datatables_options = options.datatables_options || { paging: false, bInfo: false, searching: false, scrollX: true, sScrollXInner: '100%', sScrollX: '100%' },
-        table_class = options.table_class || 'table table-hover';
+      csv_path = options.csv_path,
+      el = options.element || '#datatables',
+      allow_download = options.allow_download || false,
+      csv_options = options.csv_options || {
+        separator: ',',
+        delimiter: '"'
+      },
+      datatables_options = options.datatables_options || {
+        paging: false,
+        bInfo: false,
+        searching: false,
+        scrollX: true,
+        sScrollXInner: '100%',
+        sScrollX: '100%'
+      },
+      table_class = options.table_class || 'table table-hover';
 
     // clear:
     $(el).html('');
 
     // loop through chartInfo.
-    chartInfo.tables.forEach(function(tableData, index) {
+    chartInfo.tables.forEach(function (tableData, index) {
 
       $(el).append($('<h3 />').text(tableData.title));
 
-      if(tableData.data.length) {
+      if (tableData.data.length) {
         $(el).append($('<a />').text('Download data')
-            .attr({
-              'href': URL.createObjectURL(new Blob([that.toCsv(tableData)], { type: 'text/csv' })),
-              'download': chartInfo.indicatorId + tableData.title + '.csv',
-              'class': 'btn btn-primary'
-            })
-            .data('csvdata', that.toCsv(tableData)));
+          .attr({
+            'href': URL.createObjectURL(new Blob([that.toCsv(tableData)], {
+              type: 'text/csv'
+            })),
+            'download': chartInfo.indicatorId + tableData.title + '.csv',
+            'class': 'btn btn-primary'
+          })
+          .data('csvdata', that.toCsv(tableData)));
 
         var currentId = 'indicatortable' + index;
 
         var currentTable = $('<table />').attr({
           'class': 'table-responsive ' + table_class,
-          'id' : currentId
+          'id': currentId
         });
 
         var table_head = '<thead><tr>';
 
-        tableData.headings.forEach(function(heading) {
+        tableData.headings.forEach(function (heading) {
           table_head += '<th>' + heading + '</th>';
         });
 
@@ -148,9 +160,9 @@ var indicatorView = function(model, options) {
         currentTable.append(table_head);
         currentTable.append('<tbody></tbody>');
 
-        tableData.data.forEach(function(data) {
+        tableData.data.forEach(function (data) {
           var row_html = '<tr>';
-          tableData.headings.forEach(function(heading, index) {
+          tableData.headings.forEach(function (heading, index) {
             row_html += '<td>' + (data[index] ? data[index] : '-') + '</td>';
           });
           row_html += '</tr>';
@@ -160,7 +172,11 @@ var indicatorView = function(model, options) {
         $(el).append(currentTable);
 
         // equal width columns:
-        datatables_options.aoColumns = _.map(tableData.headings, function(h) { return { sWidth: (100/tableData.headings.length) + '%' }; });
+        datatables_options.aoColumns = _.map(tableData.headings, function (h) {
+          return {
+            sWidth: (100 / tableData.headings.length) + '%'
+          };
+        });
         currentTable.DataTable(datatables_options);
 
       } else {
