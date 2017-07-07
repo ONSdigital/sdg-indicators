cdb5df591af725a1d9ecbe3696ca54ffa56756b7
Shane Porter <shane@freshclickmedia.com>
2017-07-07 16:52:55 +0100

    load and process JSON

--- assets/js/search.js
+++ assets/js/search.js
@@ -1,3 +1,34 @@
+var indicatorSearch = function() {
+  that = this;
+  this.element = $('#indicator_search');
+  this.dataUrl = this.element.data('url');
+  this.indicatorData = [];
+  this.hasErrored = false;
+
+  this.processData = function(data) {
+    for(var goalLoop = 0; goalLoop < data.length; goalLoop++) {
+      for(var indicatorLoop = 0; indicatorLoop < data[goalLoop].goal.indicators.length; indicatorLoop++) {
+        var currentIndicator = data[goalLoop].goal.indicators[indicatorLoop];
+        currentIndicator.goalId = data[goalLoop].id;
+        currentIndicator.goalTitle = data[goalLoop].title;
+        that.indicatorData.push(currentIndicator);
+      }
+    }
+  };
+
+  $.getJSON(this.dataUrl, function(data) {
+    that.processData(data);
+  }).fail(function(err) {
+    that.hasErrored = true;
+    console.error(err);
+  });
+};
+
+indicatorSearch.prototype = {
+
+};
+
 $(function() {
-  console.log('search.js');
+  new indicatorSearch();
 });
+
