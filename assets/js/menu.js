00e7ebee81b9f0629cc81b612567609f5e366e3f
Shane Porter <shane@freshclickmedia.com>
2017-09-20 10:53:29 +0100

    responsive top level menu improvements

--- assets/js/menu.js
+++ assets/js/menu.js
@@ -8,7 +8,7 @@ $(function() {
     $('.top-level a').removeClass('open');
   };  
 
-  $('.top-level a').click(function() {
+  $('.top-level span').click(function() {
     var target = $(this).data('target');
 
     $('.top-level li').removeClass('active');
