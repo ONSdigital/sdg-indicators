e600177c314912ff3fdaae89d4e6e0fe59f4e37a
Doug Ashton <douglas.j.ashton@gmail.com>
2017-09-25 13:11:01 +0100

    one tiny tweak at a time. fix path to deploy_key

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -20,7 +20,7 @@ SHA=`git rev-parse --verify HEAD`
 # Clone the existing gh-pages for this repo into out/
 # Create a new empty branch if gh-pages doesn't exist yet (should only happen on first deply)
 
-chmod 600 ../deploy_key
+chmod 600 deploy_key
 eval `ssh-agent -s`
 ssh-add deploy_key
 
