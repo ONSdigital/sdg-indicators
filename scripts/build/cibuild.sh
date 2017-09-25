1e5c65448a5f2032274fdc2c0acb246849077f3b
Doug Ashton <douglas.j.ashton@gmail.com>
2017-09-25 20:19:47 +0100

    give cibuild a shell extension

--- scripts/build/cibuild.sh
+++ scripts/build/cibuild.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 # Instead of seperate prods just update variables that differ
 cat _config.yml | sed 's/^baseurl: .*$/baseurl: ""/' > _config_prod.yml
 jekyll build --config _config_prod.yml
