415bf5c6fd1b14a6aca2ef0c864d55177c7bc196
Doug Ashton <douglas.j.ashton@gmail.com>
2017-10-05 14:46:25 +0100

    append message to unreported stats #350

--- scripts/batch/updatemetadata.py
+++ scripts/batch/updatemetadata.py
@@ -33,8 +33,12 @@ def main():
             meta = next(yaml.safe_load_all(stream))
         # Load in md content as well?
         if is_unreported(meta, met):
-            print(met)
-
+            if meta["reporting_status"] == "notstarted":
+                txt = "\nWe are still looking for a suitable data source for this indicator. Please contact us if you think you can help by emailing <a href=\"mailto:SustainableDevelopment@ons.gov.uk\">SustainableDevelopment@ons.gov.uk</a>.\n\nIn the future we plan to split this category so we can show you which indicators represent data gaps are currently in development and which are still left to look at.\n"
+            else:
+                txt = "\nWe have found a suitable source of data for this indicator or relevant proxy at national level. We are currently quality assuring the data and preparing it for publication.\n"
+            with open(met, "a") as metafile:
+                metafile.write(txt)
 
 if __name__ == '__main__':
     main()
