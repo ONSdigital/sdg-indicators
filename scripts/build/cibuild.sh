19d75c6d30fcb05aa91fcb9600e54fa2fc8ff6fb
Douglas <douglas.j.ashton@gmail.com>
2017-10-21 00:33:40 +0100

    modify htmlproofer for develop

--- scripts/build/cibuild.sh
+++ scripts/build/cibuild.sh
@@ -6,10 +6,16 @@ python3 scripts/build/headlines.py || exit 1
 
 # Instead of seperate prods just update variables that differ
 if [ "$TRAVIS_BRANCH" = "master" ]; then
-  cat _config.yml | sed 's/^baseurl: .*$/baseurl: ""/' > _config_prod.yml
-  jekyll build --config _config_prod.yml
+  cat _config.yml | sed 's/^baseurl: .*$/baseurl: ""/' > _config_prod.yml &&
+  jekyll build --config _config_prod.yml &&
+  htmlproofer --disable-external ./_site
 else
-  jekyll build --config _config.yml
+  jekyll build --config _config.yml &&
+  mkdir -p ./test/sdg-indicators &&
+  cp -r ./_site/* ./test/sdg-indicators/ &&
+  touch ./test/index.html &&
+  htmlproofer --disable-external ./test &&
+  rm -rf ./test
 fi
 
-htmlproofer --disable-external ./_site
+
