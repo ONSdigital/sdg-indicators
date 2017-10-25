96a17b10d85058f737192e7c7feb73dd63f521be
Doug Ashton <douglas.j.ashton@gmail.com>
2017-10-25 22:23:44 +0100

    trim trailing whitespace

--- assets/js/indicatorView.js
+++ assets/js/indicatorView.js
@@ -1,22 +1,22 @@
 var indicatorView = function (model, options) {
-  
+
   "use strict";
-  
+
   var view_obj = this;
-  
+
   //this._fieldLimit = 2;
   this._model = model;
-  
+
   this._chartInstance = undefined;
   this._rootElement = options.rootElement;
-  
+
   var chartHeight = screen.height < options.maxChartHeight ? screen.height : options.maxChartHeight;
-  
-  $('.plot-container', this._rootElement) 
-  .css('height', chartHeight + 'px'); 
-  
+
+  $('.plot-container', this._rootElement)
+  .css('height', chartHeight + 'px');
+
   this._model.onDataComplete.attach(function (sender, args) {
-    
+
     if(view_obj._model.showData) {
       if(!view_obj._chartInstance) {
         view_obj.createPlot(args);
@@ -24,14 +24,14 @@ var indicatorView = function (model, options) {
         view_obj.updatePlot(args);
       }
     }
-    
+
     view_obj.createTables(args);
   });
-  
+
   this._model.onSeriesComplete.attach(function (sender, args) {
     view_obj.initialiseSeries(args);
   });
-  
+
   this._model.onSeriesSelectedChanged.attach(function (sender, args) {
     // var selector;
     // if (args.series.length === view_obj._fieldLimit) {
@@ -44,54 +44,54 @@ var indicatorView = function (model, options) {
     //   selector.parent().removeClass('disabled').removeAttr('title');
     // }
   });
-  
+
   this._model.onUnitsComplete.attach(function(sender, args) {
     view_obj.initialiseUnits(args);
   });
-  
+
   this._model.onUnitsSelectedChanged.attach(function(sender, args) {
     // update the plot's y axis label
     // update the data
   });
-  
+
   this._model.onFieldsCleared.attach(function(sender, args) {
     $(view_obj._rootElement).find(':checkbox').prop('checked', false);
     $(view_obj._rootElement).find('#clear').addClass('disabled');
-    
+
     // #246
     $(view_obj._rootElement).find('.selected').css('width', '0');
     // end of #246
   });
-  
+
   this._model.onSelectionUpdate.attach(function(sender, selectedFields) {
     $(view_obj._rootElement).find('#clear')[selectedFields.length ? 'removeClass' : 'addClass']('disabled');
-    
+
     // loop through the available fields:
     $('.variable-selector').each(function(index, element) {
       var currentField = $(element).data('field');
-      
+
       // any info?
       var match = _.findWhere(selectedFields, { field : currentField });
       var element = $(view_obj._rootElement).find('.variable-selector[data-field="' + currentField + '"]');
       var width = match ? (Number(match.values.length / element.find('.variable-options label').length) * 100) + '%' : '0';
-      
+
       $(element).find('.bar .selected').css('width', width);
     });
   });
-  
+
   this._model.onFieldsStatusUpdated.attach(function (sender, args) {
     //console.log('updating field states with: ', args);
-    
-    // reset: 
+
+    // reset:
     $(view_obj._rootElement).find('label').removeClass('selected possible excluded');
-    
+
     _.each(args.data, function(fieldGroup) {
       _.each(fieldGroup.values, function(fieldItem) {
         var element = $(view_obj._rootElement).find(':checkbox[value="' + fieldItem.value + '"][data-field="' + fieldGroup.field + '"]');
         element.parent().addClass(fieldItem.state);
       });
     });
-    
+
     _.each(args.selectionStates, function(ss) {
       // find the appropriate 'bar'
       var element = $(view_obj._rootElement).find('.variable-selector[data-field="' + ss.field + '"]');
@@ -100,25 +100,25 @@ var indicatorView = function (model, options) {
       element.find('.bar .excluded').css('width', ss.fieldSelection.excludedState + '%');
     });
   });
-  
+
   $(document).click(function(e) {
     $('.variable-options').hide();
   });
-  
+
   $(this._rootElement).on('click', '#clear', function() {
     view_obj._model.clearSelectedFields();
   });
-  
+
   $(this._rootElement).on('click', '#fields label', function (e) {
     $(this).find(':checkbox').trigger('click');
     e.preventDefault();
     e.stopPropagation();
   });
-  
+
   $(this._rootElement).on('change', '#units input', function() {
     view_obj._model.updateSelectedUnit($(this).val());
   });
-  
+
   // generic helper function, used by clear all/select all and individual checkbox changes:
   var updateWithSelectedFields = function() {
     view_obj._model.updateSelectedFields(_.chain(_.map($('#fields input:checked'), function (fieldValue) {
@@ -137,76 +137,76 @@ var indicatorView = function (model, options) {
       selected: $(this).is(':checked')
     });
   }
-  
+
   $(this._rootElement).on('click', '.variable-options button', function(e) {
     var type = $(this).data('type');
     var $options = $(this).closest('.variable-options').find(':checkbox');
-    
+
     $options.prop('checked', type == 'select');
-    
+
     updateWithSelectedFields();
-    
+
     e.stopPropagation();
   });
-  
+
   $(this._rootElement).on('click', ':checkbox', function(e) {
-    
+
     // don't permit excluded selections:
     if($(this).parent().hasClass('excluded')) {
       return;
     }
-    
+
     updateWithSelectedFields();
-    
+
     e.stopPropagation();
   });
-  
+
   $(this._rootElement).on('click', '.variable-selector', function(e) {
-    
+
     var options = $(this).find('.variable-options');
     var optionsVisible = options.is(':visible');
-    
+
     // ensure any others are hidden:
     $('.variable-options').hide();
-    
+
     // but reinstate this one:
     $(options)[optionsVisible ? 'hide' : 'show']();
-    
+
     e.stopPropagation();
   });
-  
+
   this.initialiseSeries = function (args) {
     var template = _.template($("#item_template").html());
-    
+
     $('<button id="clear" class="disabled">Clear selections <i class="fa fa-remove"></i></button>').insertBefore('#fields');
-    
+
     $('#fields').html(template({
       series: args.series
     }));
   };
-  
+
   this.initialiseUnits = function(args) {
     var template = _.template($('#units_template').html());
-    
+
     $('#units').html(template({
       units: args.units
     }));
   };
-  
+
   this.updatePlot = function(chartInfo) {
     view_obj._chartInstance.data.datasets = chartInfo.datasets;
-    
+
     if(chartInfo.selectedUnit) {
       view_obj._chartInstance.options.scales.yAxes[0].scaleLabel.labelString = chartInfo.selectedUnit;
     }
-    
+
     view_obj._chartInstance.update(1000, true);
   };
-  
+
   this.createPlot = function (chartInfo) {
-    
+
     var that = this;
-    
+
     this._chartInstance = new Chart($(this._rootElement).find('canvas'), {
       type: 'line',
       data: chartInfo,
@@ -254,24 +254,24 @@ var indicatorView = function (model, options) {
         }
       }
     });
-    
+
     Chart.pluginService.register({
       afterDraw: function(chart) {
         var $canvas = $(that._rootElement).find('canvas');
-        
+
         function putTextOutputs(textOutputs, x0, textAlign) {
           var textRowHeight = 20;
           var x = x0;
           var y = $canvas.height() - 10 - ((textOutputs.length - 1) * textRowHeight);
-          
+
           var canvas = $canvas.get(0);
           var ctx = canvas.getContext("2d");
-          
+
           ctx.textAlign = textAlign;
           ctx.textBaseline = 'middle';
           ctx.font = '12px Arial';
           ctx.fillStyle = '#6e6e6e';
-          
+
           _.each(textOutputs, function(textOutput) {
             ctx.fillText(textOutput, x, y);
             y += textRowHeight;
@@ -284,7 +284,7 @@ var indicatorView = function (model, options) {
           'Unit of Measurement: ' + (that._model.measurementUnit ? that._model.measurementUnit : '')
         ];
         putTextOutputs(textOutputsLeft, 0, 'left');
-        
+
         var textOutputsRight = [
           (that._model.footnote ? 'Footnote: ' + that._model.footnote : '')
         ];
@@ -292,28 +292,28 @@ var indicatorView = function (model, options) {
       }
     });
   };
-  
+
   this.toCsv = function (tableData) {
     var lines = [],
     headings = tableData.headings;
-    
+
     lines.push(headings.join(','));
-    
+
     _.each(tableData.data, function (dataValues) {
       var line = [];
-      
+
       _.each(headings, function (heading, index) {
         line.push(dataValues[index]);
       });
-      
+
       lines.push(line.join(','));
     });
-    
+
     return lines.join('\n');
   };
-  
+
   this.createTables = function (chartInfo) {
-    
+
     options = options || {};
     var that = this,
     csv_path = options.csv_path,
@@ -332,33 +332,33 @@ var indicatorView = function (model, options) {
       sScrollX: '100%'*/
     },
     table_class = options.table_class || 'table table-hover';
-    
+
     // clear:
     $(el).html('');
-    
+
     // loop through chartInfo.
     chartInfo.tables.forEach(function (tableData, index) {
-      
+
       $(el).append($('<h3 />').text(tableData.title));
-      
+
       if (tableData.data.length) {
         var currentId = 'indicatortable' + index;
-        
+
         var currentTable = $('<table />').attr({
           'class': 'table-responsive ' + table_class,
           'id': currentId
         });
-        
+
         var table_head = '<thead><tr>';
-        
+
         tableData.headings.forEach(function (heading) {
           table_head += '<th>' + heading + '</th>';
         });
-        
+
         table_head += '</tr></thead>';
         currentTable.append(table_head);
         currentTable.append('<tbody></tbody>');
-        
+
         tableData.data.forEach(function (data) {
           var row_html = '<tr>';
           tableData.headings.forEach(function (heading, index) {
@@ -367,7 +367,7 @@ var indicatorView = function (model, options) {
           row_html += '</tr>';
           currentTable.find('tbody').append(row_html);
         });
-        
+
         if(window.Modernizr && window.Modernizr.blobconstructor) {
           //          $(el).append($('<h5 />').text('Download Headline Data')
           //            .attr({
@@ -384,9 +384,9 @@ var indicatorView = function (model, options) {
           })
           .data('csvdata', that.toCsv(tableData)));
         }
-        
+
         $(el).append(currentTable);
-        
+
         // equal width columns:
         datatables_options.aoColumns = _.map(tableData.headings, function (h) {
           return {
@@ -394,13 +394,13 @@ var indicatorView = function (model, options) {
           };
         });
         datatables_options.aaSorting = [];
-        
+
         currentTable.DataTable(datatables_options);
-        
+
       } else {
         $(el).append($('<p />').text('There is no data for this breakdown.'));
       }
-      
+
       $(el).append('<hr />');
     });
   };
