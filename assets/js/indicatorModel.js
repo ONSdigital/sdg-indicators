ce9348c5330d6f230c2dbf97770d5c2efb0be606
Shane Porter <shane@freshclickmedia.com>
2017-12-01 08:09:32 +0000

    headline data offset calculated properly, reduced max to 13

--- assets/js/indicatorModel.js
+++ assets/js/indicatorModel.js
@@ -138,7 +138,7 @@ var indicatorModel = function (options) {
   }());
 
   var headlineColor = '777777';
-  var colors = ['0082e5', '79c3fc', '005da7', 'ff9c18', 'f47d00', 'ad8cf3', '683ec9'];
+  var colors = ['0082e5', '79c3fc', '005da7', 'ff9c18', 'f47d00', 'ad8cf3'];
 
   // allow headline + (2 x others)
   var maxDatasetCount = 2 * colors.length;
@@ -303,7 +303,7 @@ var indicatorModel = function (options) {
       getColor = function(datasetIndex) {
 
         // offset if there is no headline data:
-        if(!this.hasHeadline) {
+        if(!that.hasHeadline) {
           datasetIndex += 1;
         }
 
