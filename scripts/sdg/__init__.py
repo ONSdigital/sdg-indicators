10f8f7b809053c93b90d20d4b15ab8c66da4ad9a
Doug Ashton <douglas.j.ashton@gmail.com>
2018-03-27 12:07:44 +0100

    local libs yamlmd and sdg

--- scripts/sdg/__init__.py
+++ scripts/sdg/__init__.py
@@ -4,5 +4,5 @@ __version__ = "0.0.1"
 __author__ = "Doug Ashton <dashton@mango-solutions.com>"
 
 # Load key components
-#from .read import read_yamlmd
-#from .write import write_yamlmd
+# from .read import read_yamlmd
+# from .write import write_yamlmd
