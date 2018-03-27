295c1b36cda3e2a31db0917e1191a7f0ad479fcb
Doug Ashton <douglas.j.ashton@gmail.com>
2018-03-27 12:10:04 +0100

    add build_metadata.py

--- scripts/build/build_metadata.py
+++ scripts/build/build_metadata.py
@@ -3,56 +3,92 @@
 Created on Mon Mar 26 13:32:35 2018
 
 @author: dashton
+
+
 """
 
-from sdg.path import indicator_path # local package
-from subprocess import getstatusoutput
+import re
+import sys
+import os
+# None-standard library
 import git
+# Local modules
+sys.path.append('scripts')
+import sdg
+import yamlmd
+from sdg.path import indicator_path  # local package
 
-# %% New gitpython way
-
-
-def get_last_update(obj, repo):    
-    commit = next(repo.iter_commits(paths=obj.path, max_count=1))
-    date = str(commit.committed_datetime.date())
-    return {'file': obj.path, 'sha':commit.hexsha, 'date': date}
-
-repo = git.Repo("~/Web/sdg-indicators/data) # full path good for submodules
-tree = repo.tree()
-
-%time git_update = [get_last_update(obj, repo) for obj in tree['data']]
-
+# %% Faster but not using right now
+# 
+# def get_last_update(obj, repo):
+#     commit = next(repo.iter_commits(paths=obj.path, max_count=1))
+#     git_date = str(commit.committed_datetime.date())
+#     return {'file': obj.path, 'sha':commit.hexsha, 'date': git_date}
+# 
+# repo = git.Repo("data", search_parent_directories=True) 
+# tree = repo.tree()
+# 
+# %time git_update = [get_last_update(obj, repo) for obj in tree['data']]
+# 
 # %% Get file updates
 
 
-def get_git_update(f):
+def get_git_update(inid, ftype):
     """Change into the working directory of the file (it might be a submodule)
     and get the latest git history"""
-    
+    f = indicator_path(inid, ftype=ftype, mode='r')
     f_dir, f_name = os.path.split(f)
-    g = git.Git(f_dir)
-
-    git_date = g.log('--pretty=%ai', '-n', '1', '--', f_name)
-    git_sha = g.log('--pretty=%H', '-n', '1', '--', f_name)
-        
+    
+    repo = git.Repo(f_dir, search_parent_directories=True)
+    
+    commit = next(repo.iter_commits(paths=f, max_count=1))
+    git_date = str(commit.committed_datetime.date())
+    git_sha = commit.hexsha
+    # Turn the remote URL into a commit URL
+    remote = repo.remote().url
+    remote_bare = re.sub('^.*github\.com(:|\/)', '', remote).replace('.git','')
+    commit_url = 'https://github.com/'+remote_bare+'/commit/'+git_sha
+    
     return {'date': git_date,
             'sha': git_sha,
             'file': f,
-            'remote': 'pass'}   
+            'id': inid,
+            'commit_url': commit_url}
+ 
 
 
 
 def get_git_updates(inid):
-    print(inid)
-    meta_update = get_git_update(indicator_path(inid, ftype='meta', mode='r'))
-    data_update = get_git_update(indicator_path(inid, ftype='data', mode='r'))
+    meta_update = get_git_update(inid=inid, ftype='meta')
+    data_update = get_git_update(inid=inid, ftype='data')
     
-    return {'admin_release_date': data_update['date'],
-            'admin_release_sha': data_update['sha'],
-            'admin_meta_release_date': meta_update['date'],
-            'admin_meta_release_sha': meta_update['sha'],
+    return {'national_data_update_url_text': data_update['date'],
+            'national_data_update_url': data_update['commit_url'],
+            'national_metadata_update_url_text': meta_update['date'],
+            'national_metadata_update_url': meta_update['commit_url']
             }
 
+
+# %% Get all new metadata
+
+
+def build_meta(inid):
+    """Perform pre-processing for the metadata files"""
+    status = True
+    # Read and write paths may be different
+    fr = indicator_path(inid, ftype='meta', mode='r')
+    fw = indicator_path(inid, ftype='meta', mode='w')
+
+    meta = yamlmd.read_yamlmd(fr)
+
+    git_update = get_git_updates(inid)
+
+    for k in git_update.keys():
+        meta[0][k] = git_update[k]
+
+    yamlmd.write_yamlmd(meta, fw)
+
+    return status
 
 # %% Read each csv and run the checks
 
@@ -61,11 +97,12 @@ def main():
     """Process the metadata files ready for site build"""
     status = True
     ids = sdg.path.get_ids()
-    
+
     print("Building " + str(len(ids)) + " metadata files...")
 
     for inid in ids:
         try:
+            print(inid)
             status = status & build_meta(inid)
         except Exception as e:
             status = False
