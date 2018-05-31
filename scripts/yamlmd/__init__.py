120c28b507e0582c806f0b35f881a74f809edb41
Doug Ashton <douglas.j.ashton@gmail.com>
2018-05-31 17:11:17 +0100

    remove python

--- scripts/yamlmd/__init__.py
+++
@@ -1,8 +0,0 @@
-"""Read and write to yaml headed Markdown files"""
-
-__version__ = "0.1.4"
-__author__ = "Doug Ashton <douglas.j.ashton@gmail.com>"
-
-# Load key components
-from .read import read_yamlmd
-from .write import write_yamlmd
