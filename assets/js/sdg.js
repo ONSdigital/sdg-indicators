4e2d383e08ca052ab63e2673489f2c989c2ed22e
Travis CI <>
2019-01-31 15:18:41 +0000

    Deploy 827c40c from branch develop

--- assets/js/sdg.js
+++ assets/js/sdg.js
@@ -1168,8 +1168,8 @@ var mapView = function () {
     $('#map').sdgMap({
       geoData: geoData,
       geoCodeRegEx: geoCodeRegEx,
-      mapOptions: null,
-      mapLayers: null,
+      mapOptions: {"tileURL":"https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}","tileOptions":{"id":"mapbox.light","accessToken":"pk.eyJ1IjoiYnJvY2tmYW5uaW5nMSIsImEiOiJjaXplbmgzczgyMmRtMnZxbzlmbGJmdW9pIn0.LU-BYMX69uu3eGgk0Imibg","attribution":"<a href=\"https://www.mapbox.com\">Mapbox</a> | <a href=\"http://geoportal.statistics.gov.uk/\">ONS</a>"},"minZoom":6},
+      mapLayers: [{"min_zoom":0,"max_zoom":6,"serviceUrl":"https://opendata.arcgis.com/datasets/4fcca2a47fed4bfaa1793015a18537ac_4.geojson","nameProperty":"rgn17nm","idProperty":"rgn17cd","staticBorders":true},{"min_zoom":7,"max_zoom":20,"serviceUrl":"https://geoportal1-ons.opendata.arcgis.com/datasets/686603e943f948acaa13fb5d2b0f1275_4.geojson","nameProperty":"lad16nm","idProperty":"lad16cd"}],
     });
   };
 };
