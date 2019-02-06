94d70b1dc4269f6dd4ecf414f128c515962b5172
Douglas Ashton <douglas.j.ashton@gmail.com>
2019-02-06 21:23:05 +0000

    only build PRs

--- scripts/cibuild.sh
+++ scripts/cibuild.sh
@@ -1,18 +1,25 @@
 #!/bin/bash
 
+echo "TRAVIS_BRANCH = " $TRAVIS_BRANCH
+echo "TRAVIS_PULL_REQUEST = " $TRAVIS_PULL_REQUEST
+echo "TRAVIS_PULL_REQUEST_BRANCH = " $TRAVIS_PULL_REQUEST_BRANCH
+
+
 # Instead of seperate prods just update variables that differ
 if [ "$TRAVIS_BRANCH" = "master" ]; then
   jekyll build --config _config_prod.yml &&
   htmlproofer --disable-external ./_site
 else
 
-  if [ "$TRAVIS_BRANCH" = "develop" ]; then
-    BASEURL="sdg-indicators"
-  else
-    BASEURL=$(echo "$TRAVIS_BRANCH" | iconv -t ascii//TRANSLIT | sed -r s/[^a-zA-Z0-9]+/-/g | sed -r s/^-+\|-+$//g | tr A-Z a-z)
+  if [ "$TRAVIS_PULL_REQUEST" = "true" ]; then
+    BASEURL=$TRAVIS_PULL_REQUEST_BRANCH
+    # slugify
+    BASEURL=$(echo "$BASEURL" | iconv -t ascii//TRANSLIT | sed -r s/[^a-zA-Z0-9]+/-/g | sed -r s/^-+\|-+$//g | tr A-Z a-z)
     sed -i 's|/sdg-indicators|/'$BASEURL'|g' _config.yml
+  else
+    BASEURL="sdg-indicators"
   fi
-
+  
   jekyll build --config _config.yml &&
   # The following is because htmlproofer doesn't like the /sdg-indicators baseurl
   mkdir -p ./_test/$BASEURL &&
--- scripts/deploy/deploy_testing.sh
+++ scripts/deploy/deploy_testing.sh
@@ -2,14 +2,18 @@
 set -e # Exit with nonzero exit code if anything fails
 
 echo "TRAVIS_BRANCH = " $TRAVIS_BRANCH
+echo "TRAVIS_PULL_REQUEST_BRANCH = " $TRAVIS_PULL_REQUEST_BRANCH
 
 echo "TRAVIS_TAG = " $TRAVIS_TAG
 
 # Pull requests and commits to other branches shouldn't try to deploy, just build to verify
-#if [ "$TRAVIS_PULL_REQUEST" != "false" -o "$TRAVIS_BRANCH" != "$SOURCE_BRANCH" ]; then
-#    echo "Skipping deploy; just doing a build."
-#    exit 0
-#fi
+if [ "$TRAVIS_PULL_REQUEST" = "true" ]; then
+  BASEURL=$TRAVIS_PULL_REQUEST_BRANCH
+  # slugify
+  BASEURL=$(echo "$BASEURL" | iconv -t ascii//TRANSLIT | sed -r s/[^a-zA-Z0-9]+/-/g | sed -r s/^-+\|-+$//g | tr A-Z a-z)
+else
+  BASEURL="sdg-indicators"
+fi
 
 # Keys
 openssl aes-256-cbc -K $encrypted_6a5f295aa62e_key -iv $encrypted_6a5f295aa62e_iv -in scripts/deploy/keys.tar.enc -out scripts/deploy/keys.tar -d
@@ -20,10 +24,6 @@ chmod 600 ./scripts/deploy/deploy_key_test
 eval `ssh-agent -s`
 ssh-add scripts/deploy/deploy_key_test
 rm scripts/deploy/deploy_key*
-
-# Make a safe directory name (slugify)
-BASEURL=$(echo "$TRAVIS_BRANCH" | iconv -t ascii//TRANSLIT | sed -r s/[^a-zA-Z0-9]+/-/g | sed -r s/^-+\|-+$//g | tr A-Z a-z)
-echo "Directory on test server = " $BASEURL
 
 # Push the files over, removing anything existing already.
 ssh -oStrictHostKeyChecking=no $TEST_USER_SERVER "rm -rf ~/www/$BASEURL || true"
