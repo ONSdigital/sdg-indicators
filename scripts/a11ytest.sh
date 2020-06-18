3e369dfe97b94647d96a7fb082a1063c59bc54fb
SavvasStephanides <savvascyp@hotmail.com>
2020-06-18 18:46:07 +0100

    Test

--- scripts/a11ytest.sh
+++ scripts/a11ytest.sh
@@ -1,9 +1,11 @@
-# docker build -t buildsite ./a11ytest/.
-# docker run --rm -v $PWD:/site buildsite
-# docker run --rm \
-#     -p 80:80 \
-#     -v $PWD/_site:/usr/local/apache2/htdocs/sdg-indicators \
-#     --name jekyllbuildserver \
-#     httpd:2.4.41-alpine
+docker build -t buildsite ./a11ytest/.
+docker run --rm -v $PWD:/site buildsite
+docker run --rm \
+    -p 80:80 \
+    -v $PWD/_site:/usr/local/apache2/htdocs/sdg-indicators \
+    --name jekyllbuildserver \
+    httpd:2.4.41-alpine
 
 npm install -g pa11y-ci
+
+pa11y-ci --config ./a11ytest/pa11y-desktop.json
