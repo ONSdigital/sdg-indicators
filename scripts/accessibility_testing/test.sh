bedacb069b6e4ff6e9145b878ea380ee98c1adf4
SavvasStephanides <savvascyp@hotmail.com>
2020-06-22 16:37:17 +0100

    Add Pa11y to Github Actions

---
+++ scripts/accessibility_testing/test.sh
@@ -0,0 +1,3 @@
+npx pa11y-ci --config ./scripts/accessibility_testing/configurations/desktop.json
+npx pa11y-ci --config ./scripts/accessibility_testing/configurations/mobile.json
+npx pa11y-ci --config ./scripts/accessibility_testing/configurations/contrast.json
