20c19c06f984f298d3cf55ea90f7c020b1832b55
SavvasStephanides <savvascyp@hotmail.com>
2020-06-24 11:37:55 +0100

    Add accessibility testing to Github Actions

--- scripts/accessibility-test/index.js
+++ scripts/accessibility-test/index.js
@@ -1,11 +1,8 @@
 const pa11y = require('pa11y');
 const assert = require("assert")
 
-async function runAccessibilityTests(){
-    var results = await pa11y("http://example.com")
+pa11y("http://example.com").then((results) => {
     var issues = results.issues
-    
-    assert.deepEqual(issues.length, 0)
-}
 
-runAccessibilityTests()
+    assert.deepEqual(issues.length, 0)
+})
