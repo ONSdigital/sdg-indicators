e2a21630a70022c5637eff64662b5f0fc42a3829
SavvasStephanides <savvascyp@hotmail.com>
2020-06-24 11:52:06 +0100

    Add accessibility testing to Github Actions

--- scripts/accessibility-test/test/index.js
+++ scripts/accessibility-test/test/index.js
@@ -5,7 +5,7 @@ const { doesNotMatch } = require('assert');
 describe("example.com", function(){
     describe("Accesibility test", function(){
         it("should pass with no issues", async function(){
-            var result = await pa11y("http://example.com")
+            var result = await pa11y("https://sustainabledevelopment-uk.github.io/")
             var issues = result.issues
 
             if(issues.length > 0){
