9f90491ff3460e8fe86cddfd1bb77cec01ee7de6
SavvasStephanides <savvascyp@hotmail.com>
2020-06-24 15:00:00 +0100

    Pa11y tests

--- scripts/accessibility-test/run_tests.sh
+++ scripts/accessibility-test/run_tests.sh
@@ -1,3 +1,6 @@
+echo "Desktop test"
 pa11y ./_site/$pagePath/index.html
+echo "Mobile test"
 pa11y --config ./scripts/config/mobile.json ./_site/$pagePath/index.html
+echo "High contrast test"
 pa11y --config ./scripts/config/high-contrast.json ./_site/$pagePath/index.html
