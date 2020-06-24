018fbc216c2d80d3e725e5735cae176942583774
SavvasStephanides <savvascyp@hotmail.com>
2020-06-24 12:30:15 +0100

    Add accessibility testing to Github Actions

--- scripts/accessibility-test/test/index.js
+++ scripts/accessibility-test/test/index.js
@@ -14,7 +14,7 @@ var configurations = {
         ],
         "chromeLaunchConfig": {
             "args": ["--no-sandbox"]
-        } 
+        }
     },
     "mobile": {
         "viewport": {
@@ -24,23 +24,26 @@ var configurations = {
     }
 }
 
-sites.forEach((site) => {
-    Object.keys(configurations).forEach((configuration) => {
-        describe(`${site} on ${configuration}`, () => {
-            it("Accessibility test should pass with no issues", (done) => {
+for (var i = 0; i < sites.length; i++) {
+    var site = sites[i]
+    for (var a = 0; a < Object.keys(configurations).length; a++) {
+        var configurationName = Object.keys(configurations)[a]
+        var configuration = configurations[configurationName]
+
+        describe(`${site} on ${configurationName}`, () => {
+            it("should pass accessibility tests", (done) => {
                 pa11y(site, configuration)
-                .then((results) => {
-                    var issues = results.issues
+                .then((result) => {
+                    var issues = result.issues
+
                     if(issues.length > 0){
-                        console.log(issues);
+                        console.log(issues)
                         assert.fail()
                     }
                     done()
                 })
                 .catch(done)
-                
             })
         })
-    })
-    
-})
+    }
+}
