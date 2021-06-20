8c76876708e0837bbeef2d5111d58ddaf08f813b
Brock Fanning <brockfanning@gmail.com>
2021-06-20 18:19:04 -0400

    Remove unneeded config

--- _includes/cookies-config.js
+++ _includes/cookies-config.js
@@ -1,39 +1,19 @@
 var klaroConfig = {
-    version: 1,
-    elementID: 'klaro',
-    styling: {
-        theme: ['light', 'top', 'wide'],
-    },
     noAutoLoad: true, // no autoload because we have a custom notice and form
-    htmlTexts: false,
-    embedded: false,
-    groupByPurpose: true,
     storageMethod: 'cookie',
-    cookieName: 'klaro',
+    cookieName: 'cookie_settings',
     cookieExpiresAfterDays: 365,
     default: false,
-    mustConsent: false,
-    acceptAll: true,
-    hideDeclineAll: false,
-    hideLearnMore: false,
-    noticeAsModal: false,
     services: [
         {
             name: 'contrast',
             default: true,
-            title: 'High-contrast mode',
-            purposes: ['functional'],
             cookies: ['contrast'],
             required: true,
-            optOut: false,
-            onlyOnce: true,
         },
         {% if site.analytics.ga_prod and site.analytics.ga_prod != '' %}
         {
             name: 'google-analytics',
-            title: 'Google Analytics',
-            default: false,
-            purposes: ['analytics'],
             cookies: ['_gat', '_gid', 'ga'],
             callback: function(consent, service) {
                 if (consent) {
@@ -41,9 +21,6 @@ var klaroConfig = {
                     initialiseGoogleAnalytics();
                 }
             },
-            required: false,
-            optOut: false,
-            onlyOnce: false,
         },
         {% endif %}
     ],
