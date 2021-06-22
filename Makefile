5fbfde822dd9c494171aac8bb90dce8814fb237b
Brock Fanning <brockfanning@gmail.com>
2021-06-22 12:44:44 -0400

    Cookie consent form with GDS

---
+++ Makefile
@@ -0,0 +1,19 @@
+.PHONY: install build serve
+
+install:
+	npm install
+	bundle install
+	cp -r node_modules/govuk-frontend/govuk/assets/images assets/
+	cp -r node_modules/govuk-frontend/govuk/assets/fonts assets/
+	cp node_modules/govuk-frontend/govuk/all.js assets/js/govuk.js
+
+build.staging:
+	sass assets/css/govuk_staging.scss assets/css/govuk.css
+	bundle exec jekyll build
+
+build.production:
+	sass assets/css/govuk_production.scss assets/css/govuk.css
+	bundle exec jekyll build --config _config.yml,_config_prod.yml
+
+serve: build.staging
+	bundle exec jekyll serve --skip-initial-build
---
+++ _includes/custom/cookies.js
@@ -0,0 +1,69 @@
+var manager = klaro.getManager(),
+    consents = manager.loadConsents(),
+    $cookieBanner = $('#cookie-banner'),
+    $confirmationAccept = $('#cookie-banner-accept'),
+    $confirmationReject = $('#cookie-banner-reject'),
+    $cookiePageSubmit = $('#cookie-page-submit');
+
+if (!manager.confirmed) {
+  $cookieBanner.show();
+  $('#cookie-accept').click(function() {
+    if (typeof consents['google-analytics'] !== 'undefined') {
+      consents['google-analytics'] = true;
+    }
+    manager.saveAndApplyConsents();
+    $cookieBanner.hide();
+    $confirmationAccept.show();
+  });
+  $('#cookie-reject').click(function() {
+    if (typeof consents['google-analytics'] !== 'undefined') {
+      consents['google-analytics'] = false;
+    }
+    manager.saveAndApplyConsents();
+    $cookieBanner.hide();
+    $confirmationReject.show();
+  });
+  $('#hide-accept').click(function(e) {
+    e.preventDefault();
+    $confirmationAccept.hide();
+  });
+  $('#hide-reject').click(function(e) {
+    e.preventDefault();
+    $confirmationReject.hide();
+  });
+}
+
+if ($cookiePageSubmit.length > 0) {
+  var $analyticsYes = $('#analytics-cookies'),
+      $cookiePageSuccess = $('#cookie-page-success'),
+      $cookiePageGoBack = $('#cookie-page-go-back'),
+      saveCookieSettings = function(e) {
+        e.preventDefault();
+        if (typeof consents['google-analytics'] !== 'undefined') {
+          consents['google-analytics'] = Boolean($analyticsYes.prop('checked'));
+        }
+        manager.saveAndApplyConsents();
+        $cookiePageSuccess.show();
+        $([document.documentElement, document.body]).animate({
+          scrollTop: $cookiePageSuccess.offset().top
+        }, 500, 'swing', function() { $cookiePageGoBack.focus(); });
+      };
+
+  // Set pre-selected options.
+  if (typeof consents['google-analytics'] !== 'undefined') {
+    $analyticsYes.prop('checked', Boolean(consents['google-analytics']));
+  }
+
+  $cookiePageSubmit.click(saveCookieSettings);
+
+  // For semantics and accessibility, wrap the page in a form and add
+  // a submit handler.
+  $('#main-content > div').first().wrap('<form id="cookie-page-form" novalidate></form>');
+  $('#cookie-page-form').submit(saveCookieSettings);
+
+  // Go back behavior.
+  $cookiePageGoBack.click(function(e) {
+    e.preventDefault();
+    window.history.back();
+  });
+}
