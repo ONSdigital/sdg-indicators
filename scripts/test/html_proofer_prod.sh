03be8cd9ec84d069b0078ce56ee7ae9d98811092
Brock Fanning <brockfanning@gmail.com>
2021-06-22 13:32:10 -0400

    Allow hash href and update other workflow

--- scripts/test/html_proofer_prod.sh
+++ scripts/test/html_proofer_prod.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 # The following assumes the site is already built at _site.
-bundle exec htmlproofer --disable-external ./_site
+bundle exec htmlproofer --allow-hash-href --disable-external ./_site
