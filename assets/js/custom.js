9fcaf100d6fe504242dea97e2996d0d11944e9b0
Lucy <43404358+LucyGwilliamAdmin@users.noreply.github.com>
2022-03-22 14:39:47 +0000

    Update custom.js

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -7,6 +7,9 @@ opensdg.dataRounding = function(value) {
   if (value == null) {
     return value
   }
+  else if (Math.trunc(value).toString().length > 5) {
+    return Number(value.toPrecision())
+  }
   else {
     return Number(value.toPrecision(3))
   }
