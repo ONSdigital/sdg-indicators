52eeed12596c2735f1ef511d6e1ba00be50c916a
otis-bath <90769108+otis-bath@users.noreply.github.com>
2022-10-14 11:45:11 +0100

    Update custom.js

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -2,8 +2,8 @@
 # Do not delete this line.
 ---
 opensdg.dataRounding = function(value, context) {
-    // Round to 5 decimal places in indicator 16.2.2.
-    if (context.indicatorId === 'indicator_16-2-2') {
+    // Round to 5 decimal places in indicator 16.1.1.
+    if (context.indicatorId === 'indicator_16-1-1') {
         return Math.round(value * 100000) / 100000;
     }
     // Otherwise round to 2 decimal places.
