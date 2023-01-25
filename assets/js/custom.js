d8c7dd2bbc931b32d57cacc5ed142724420c440e
Tom-McNulty <116816619+Tom-McNulty@users.noreply.github.com>
2023-01-25 12:23:14 +0000

    Update custom.js

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,10 +1,17 @@
 ---
 # Do not delete this line.
 ---
-opensdg.dataRounding = function(value) {
-  if (value == null) {
-    return value
-  }
-  else {
-    return Number(value.toPrecision(4))
-   
+ opensdg.dataRounding = function(value, context) {
+    // Round to 4 decimal places in indicator 16.1.1.
+    if (context.indicatorId === 'indicator_16-1-1') {
+        return Math.round(value * 10000) / 1;
+    // Round to 4 decimal places in indicator 8.8.1.
+    if (context.indicatorId === 'indicator_8-8-1') {
+        return Math.round(value * 10) / 10;
+    }
+    // Otherwise round to 2 decimal places.
+    else {
+        return Math.round(value * 100) / 100;
+    }
+}
+    
