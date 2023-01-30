0e996ffc6c2f2e10f1e4ff0c510b790f1f10c8ae
Tom-McNulty <116816619+Tom-McNulty@users.noreply.github.com>
2023-01-30 15:00:57 +0000

    Update custom.js

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -7,11 +7,11 @@
         return Math.round(value * 10000) / 1;
     // Round to 4 decimal places in indicator 9.2.1.
     if (context.indicatorId === 'indicator_9-2-1') {
-        return Math.round(value * 100) / 10;
+        return Math.round(value * 1000) / 10;
     }
     // Otherwise round to 2 decimal places.
     else {
-        return Math.round(value * 100.0000) / -1;
+        return Math.round(value * 1000) / 1000;
     }
 }
     
