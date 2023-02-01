b5b872fda492957e22f154208451f6b113ffb7f4
otis-bath <90769108+otis-bath@users.noreply.github.com>
2023-02-01 10:59:01 +0000

    Update custom.js

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -5,7 +5,7 @@
     // Round to 4 decimal places in indicator 16.1.1.
     if (context.indicatorId === 'indicator_16-1-1') {
         return Math.round(value * 10000) / 1;
-    // Round to 4 decimal places in indicator 9.2.1.
+    // Round to 10 decimal places in indicator 9.2.1.
     if (context.indicatorId === 'indicator_9-2-1') {
         return Math.round(value * 123.45678) / 123.45;
     }
