e0b7ac702028369a3d521d16d8d292cad2fbea3a
Tom-McNulty <116816619+Tom-McNulty@users.noreply.github.com>
2023-02-16 11:27:47 +0000

    Update custom.js

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -3,8 +3,8 @@
 ---
  opensdg.dataRounding = function(value, context) {
     // Round to 4 decimal places in indicator 9.2.1.
-    if (context.indicatorId === 'indicator_5-3-2') {
-        return Math.round(value * 1000) / 1000;
+    if (context.indicatorId === 'indicator_8-a-1') {
+        return Math.round(value * 0.0001) / 0.0001;
        }
     // Otherwise round to 2 decimal places.
     else {
