800320ddcd6cd9972dec8d45f84b3305ab99e873
Tom-McNulty <116816619+Tom-McNulty@users.noreply.github.com>
2023-02-22 11:18:26 +0000

    Create custom.js

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -4,11 +4,11 @@
  opensdg.dataRounding = function(value, context) {
     // Round to 4 SF in indicator 5.3.2.
     if (context.indicatorId === 'indicator_5-3-2') {
-        return return Number(value.toPrecision(4))
+        return Number(value.toPrecision(4))
        }
-}
     // Otherwise round to 3 decimal places.
     else {
-        return Number(value.toPrecision(3))
+        return Number(value.toPrecision(4))
     }
+}
     
