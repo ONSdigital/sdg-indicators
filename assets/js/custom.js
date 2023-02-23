d839f69639abd1f08bf169020a593f9a96c799be
Tom-McNulty <116816619+Tom-McNulty@users.noreply.github.com>
2023-02-23 13:53:46 +0000

    Update custom.js

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -2,13 +2,30 @@
 # Do not delete this line.
 ---
  opensdg.dataRounding = function(value, context) {
-    // Round to 4 SF in indicator 5.3.2.
-    if (context.indicatorId === 'indicator_5-3-2') {
-        return Number(value.toPrecision(4))
+ function countDigitsBeforeDecimal(number) {
+  // Convert the number to a string
+  var numberString = number.toString();
+  
+  // Find the position of the decimal point
+  var decimalPosition = numberString.indexOf('.');
+  
+  // Extract the digits before the decimal point
+  var digitsBeforeDecimal = numberString.slice(0, decimalPosition);
+  
+  // Count the number of digits
+  var numDigits = digitsBeforeDecimal.length;
+ 
+  // Round to whole number
+ if (numDigits >= 4) 
+  return Number(value.toPrecision(numDigits)
        }
+                }            
+    
     // Otherwise round to 3 SF.
     else {
         return Number(value.toPrecision(3))
     }
+}
+
 }
     
