14eab46e482b3d58f1e84c37be7c6b61c3b890db
Tom-McNulty <116816619+Tom-McNulty@users.noreply.github.com>
2023-02-27 17:02:50 +0000

    Update custom.js

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,14 +1,14 @@
 ---
 # Do not delete this line.
 ---
-{% include custom/cookies.js %}
-
-opensdg.dataRounding = function(value, context) {
-   var digitsBeforeDecimal = String(value).split('.')[0].length;
-   if (digitsBeforeDecimal > 1) {
-       return Number(value.toPrecision(digitsBeforeDecimal))
-   }
-   else {
-       return Number(value.toPrecision(3))
-   }
+ opensdg.dataRounding = function(value, context) {
+    // Round to 4 SF in indicator 5.3.2.
+    if (context.indicatorId === 'indicator_5-3-2') {
+        return Number(value.toPrecision(4))
+       }
+    // Otherwise round to 3 SF.
+    else {
+        return Number(value.toPrecision(3))
+    }
 }
+    
