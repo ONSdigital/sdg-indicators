9206cbfac90132bfc3c036458c33029ff370b7c8
Tom-McNulty <116816619+Tom-McNulty@users.noreply.github.com>
2023-03-01 15:51:30 +0000

    Update custom.js

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -2,7 +2,10 @@
 # Do not delete this line.
 ---
  opensdg.dataRounding = function(value, context) {
-    // Round to 4 SF in indicator 5.3.2.
+    if (value == null) {
+    return value
+  }
+ // Round to 4 SF in indicator 5.3.2.
     if (context.indicatorId === 'indicator_5-3-2') {
         return Number(value.toPrecision(4))
        }
